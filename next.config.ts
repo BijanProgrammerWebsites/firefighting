597ebf862f4b42af4bb51661b60004235a06ad7b
Alireza Binesh <46135885+Alirezab-2000@users.noreply.github.com>
2026-01-30 16:44:54 +0330

    2 Setup Mantine (#1)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+      "@mantine/charts",
+      "@mantine/form",
+      "@mantine/modals",
+      "@mantine/notifications",
+    ],
+  },
 };
 
 export default nextConfig;
