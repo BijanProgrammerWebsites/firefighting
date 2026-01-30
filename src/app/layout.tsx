f7a5df7706cde98ad30755a5ee535f06228e59e0
Alirezab-2000 <alireza.binesh.2000@gmail.com>
2026-01-30 15:18:35 +0330

    [ADD] mantine, rtl direction, theme font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,19 @@ import { PropsWithChildren, ReactNode } from "react";
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 
+import {
+  ColorSchemeScript,
+  DirectionProvider,
+  MantineProvider,
+  createTheme,
+  mantineHtmlProps,
+} from "@mantine/core";
+
 import "./globals.css";
+import "@mantine/charts/styles.css";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
 const vazirmatn = Vazirmatn({
   variable: "--font-vazirmatn",
@@ -17,9 +29,25 @@ export const metadata: Metadata = {
 type Props = PropsWithChildren;
 
 export default function RootLayout({ children }: Props): ReactNode {
+  const theme = createTheme({
+    fontFamily: vazirmatn.style.fontFamily,
+  });
   return (
-    <html lang="en" dir="ltr" className={vazirmatn.variable}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      dir="rtl"
+      {...mantineHtmlProps}
+      className={vazirmatn.variable}
+    >
+      <head>
+        <title>Firefighting</title>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <DirectionProvider>
+          <MantineProvider theme={theme}>{children}</MantineProvider>
+        </DirectionProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
 
+import { Text } from "@mantine/core";
+
 import styles from "./page.module.css";
 
 export default function Home(): ReactNode {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <Text>سلام رفیق!</Text>
+    </div>
+  );
 }
