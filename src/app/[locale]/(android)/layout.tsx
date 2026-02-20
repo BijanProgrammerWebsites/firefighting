3e79cb5b7e5c254ff9fdc6c7b5b273cccf606226
BijanProgrammer <bijaneisapour@gmail.com>
2026-02-20 09:57:12 +0330

    [ADD] android guard

--- src/app/[locale]/(android)/layout.tsx
+++ src/app/[locale]/(android)/layout.tsx
@@ -1,11 +1,16 @@
 import { PropsWithChildren, ReactNode } from "react";
 
 import ShellComponent from "@/android/components/shell/shell.component";
+import AndroidGuard from "@/android/guards/android.guard";
 
 type Props = PropsWithChildren;
 
 export default async function AndroidLayout({
   children,
 }: Props): Promise<ReactNode> {
-  return <ShellComponent>{children}</ShellComponent>;
+  return (
+    <AndroidGuard>
+      <ShellComponent>{children}</ShellComponent>
+    </AndroidGuard>
+  );
 }
