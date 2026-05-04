0e1bc4aa254cffa851324a52b73758f7b29ff9f3
BijanProgrammer <bijaneisapour@gmail.com>
2026-05-04 10:17:54 +0330

    [ADD] toolbar to dashboard

--- src/app/[locale]/(android)/(dashboard)/page.tsx
+++ src/app/[locale]/(android)/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 
 import { getTranslations } from "next-intl/server";
 
+import ToolbarComponent from "@/components/toolbar/toolbar.component";
+
 import { generateDynamicMetadata } from "@/utils/metadata.utils";
 
 import styles from "./page.module.css";
@@ -11,5 +13,9 @@ export const generateMetadata = generateDynamicMetadata("DashboardPage");
 export default async function DashboardPage(): Promise<ReactNode> {
   const t = await getTranslations("DashboardPage");
 
-  return <div className={styles.dashboard}>{t("title")}</div>;
+  return (
+    <div className={styles.dashboard}>
+      <ToolbarComponent title={t("title")} />
+    </div>
+  );
 }
