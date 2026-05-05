836bcae105011651f25fbb7a34ce107218eacaf4
Alirezab-2000 <alireza.binesh.2000@gmail.com>
2026-05-05 11:43:15 +0330

    [ADD] chart 13 to 20

--- src/app/[locale]/(android)/(dashboard)/page.tsx
+++ src/app/[locale]/(android)/(dashboard)/page.tsx
@@ -12,6 +12,13 @@ import Chart9 from "@/android/(dashboard)/charts/9";
 import Chart10 from "@/android/(dashboard)/charts/10";
 import Chart11 from "@/android/(dashboard)/charts/11";
 import Chart12 from "@/android/(dashboard)/charts/12";
+import Chart13 from "@/android/(dashboard)/charts/13";
+import Chart14 from "@/android/(dashboard)/charts/14";
+import Chart15 from "@/android/(dashboard)/charts/15";
+import Chart16 from "@/android/(dashboard)/charts/16";
+import Chart17 from "@/android/(dashboard)/charts/17";
+import Chart18 from "@/android/(dashboard)/charts/18";
+import Chart19 from "@/android/(dashboard)/charts/19";
 import Chart3 from "@/android/(dashboard)/charts/chart3.chart";
 import Chart4 from "@/android/(dashboard)/charts/chart4.chart";
 import Chart5 from "@/android/(dashboard)/charts/chart5.chart";
@@ -42,6 +49,14 @@ export default async function DashboardPage(): Promise<ReactNode> {
       <Chart10 />
       <Chart11 />
       <Chart12 />
+      <Chart13 />
+      <Chart14 />
+      <Chart15 />
+      <Chart16 />
+      <Chart17 />
+      <Chart18 />
+      <Chart19 />
+      <Chart10 />
     </div>
   );
 }
