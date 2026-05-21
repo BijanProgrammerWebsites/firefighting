3cb019ab8da294c4e00a311e627bee43cc85ceec
BijanProgrammer <bijaneisapour@gmail.com>
2026-05-21 20:23:43 +0330

    [ADD] bar chart

---
+++ src/app/[locale]/(android)/(dashboard)/charts/inspections-overview-bar.chart.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { useTranslations } from "next-intl";
+
+import { useMantineTheme } from "@mantine/core";
+
+import { EChartsOption } from "echarts";
+
+import { FindAllBucketsDto } from "@/api/equipments/find-all-buckets.api";
+
+import ChartComponent from "@/components/chart/chart.component";
+
+type Props = {
+  data: FindAllBucketsDto;
+};
+
+export default function InspectionsOverviewBarChart({
+  data,
+}: Props): ReactNode {
+  const t = useTranslations("DashboardPage");
+
+  const theme = useMantineTheme();
+
+  const colorIndex = 5;
+
+  const options: EChartsOption = {
+    grid: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    legend: {
+      show: true,
+    },
+    xAxis: {
+      type: "value",
+      interval: 1,
+    },
+    yAxis: {
+      type: "category",
+      inverse: true,
+      data: [
+        t("withoutHistory"),
+        t("overdue"),
+        t("today"),
+        t("next7Days"),
+        t("next30Days"),
+      ],
+    },
+    series: [
+      {
+        type: "bar",
+        data: [
+          {
+            value: 1, //data.withoutHistory.length,
+            itemStyle: { color: theme.colors.gray[colorIndex] },
+          },
+          {
+            value: data.overdue.length,
+            itemStyle: { color: theme.colors.red[colorIndex] },
+          },
+          {
+            value: data.today.length,
+            itemStyle: { color: theme.colors.orange[colorIndex] },
+          },
+          {
+            value: data.next7Days.length,
+            itemStyle: { color: theme.colors.yellow[colorIndex] },
+          },
+          {
+            value: data.next30Days.length,
+            itemStyle: { color: theme.colors.lime[colorIndex] },
+          },
+        ],
+      },
+    ],
+  };
+
+  return <ChartComponent option={options} />;
+}
