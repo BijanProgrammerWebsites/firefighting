94c19935dfc0db5ba3b2f298e190376eacac65ea
BijanProgrammer <bijaneisapour@gmail.com>
2026-05-22 09:55:00 +0330

    [REMOVE] unnecessary page css file

--- src/app/[locale]/(auth)/sign-in/page.tsx
+++ src/app/[locale]/(auth)/sign-in/page.tsx
@@ -2,13 +2,11 @@ import { type ReactNode } from "react";
 
 import { getTranslations } from "next-intl/server";
 
-import { Text, Title } from "@mantine/core";
+import { Box, Stack, Text, Title } from "@mantine/core";
 
 import SignInFormComponent from "@/auth/sign-in/components/sign-in-form/sign-in-form.component";
 
 import { generateDynamicMetadata } from "@/utils/metadata.utils";
-
-import styles from "./page.module.css";
 
 export const generateMetadata = generateDynamicMetadata("SignInPage");
 
@@ -16,12 +14,12 @@ export default async function Page(): Promise<ReactNode> {
   const t = await getTranslations("SignInPage");
 
   return (
-    <div className={styles["sign-in"]}>
-      <div className={styles.writings}>
+    <Box mt="4rem">
+      <Stack gap="sm" align="center">
         <Title order={1}>{t("title")}</Title>
         <Text>{t("subtitle")}</Text>
-      </div>
+      </Stack>
       <SignInFormComponent />
-    </div>
+    </Box>
   );
 }
